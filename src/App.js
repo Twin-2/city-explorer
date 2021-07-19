cdcbc8db87444edd87ad4d851ceccf92a9256957
Twin-2 <davidwhit818@gmail.com>
2021-07-19 16:11:45 -0700

    lat and lon working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import axios from 'axios';
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      locationData: {},
+      formInput: ''
+    }
+  }
+
+    handleInput = (e) => {
+      e.preventDefault();
+      this.setState({formInput: e.target.value})
+    }
+
+    getLocation = async () => {
+      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_ACCESS_KEY}&q=${this.state.formInput}&format=json`;
+      const response = await axios.get(API);
+      console.log(response.data)
+      this.setState({ locationData: response.data[0]})
+    }
 
   render(){
     return(
       <>
-      <h1>Hello World!</h1>
+      <input type="text" onChange={this.handleInput} placeholder='Enter City Name Here'></input>
+      <Button varient="primary" onClick={this.getLocation}>Explore!</Button>
+      <h1>Latitude: {this.state.locationData.display_name}</h1>
+      <p>Latitude: {this.state.locationData.lat}</p>
+      <p>Longitude: {this.state.locationData.lon}</p>
       </>
     )
   }
