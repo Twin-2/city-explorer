55acc94e4d5b5b2b43856567ee8da05030833a02
Twin-2 <davidwhit818@gmail.com>
2021-07-19 18:04:55 -0700

    finished lab 06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Image, Alert} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -9,7 +9,10 @@ class App extends React.Component{
     super(props);
     this.state = {
       locationData: {},
-      formInput: ''
+      formInput: '',
+      map:'',
+      showAlert: false,
+      errors: ''
     }
   }
 
@@ -18,25 +21,49 @@ class App extends React.Component{
       this.setState({formInput: e.target.value})
     }
 
-    getLocation = async () => {
-      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_ACCESS_KEY}&q=${this.state.formInput}&format=json`;
+    getLocation = async (e) => {
+      e.preventDefault();
+      try{const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_ACCESS_KEY}&q=${this.state.formInput}&format=json`;
       const response = await axios.get(API);
       console.log(response.data)
       this.setState({ locationData: response.data[0]})
+      
+      const MAP = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ACCESS_KEY}&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=12`;
+      const mapResponse = await axios.get(MAP);
+      console.log(mapResponse.config.url)
+      this.setState({ map: mapResponse.config.url, showAlert: false})
+      }
+      catch(error){
+        console.log(error.response.status)
+        this.setState({errors: error.response.status, showAlert: true, map: '', locationData: {}})
+      }
     }
 
   render(){
     return(
-      <>
-      <input type="text" onChange={this.handleInput} placeholder='Enter City Name Here'></input>
-      <Button varient="primary" onClick={this.getLocation}>Explore!</Button>
-      <h1>Latitude: {this.state.locationData.display_name}</h1>
-      <p>Latitude: {this.state.locationData.lat}</p>
-      <p>Longitude: {this.state.locationData.lon}</p>
-      </>
+      <div id="contentBody">
+        <Alert 
+          show={this.state.showAlert} 
+          variant='warning'
+          // onClose={()=> this.setState({showAlert: false})}
+          >
+          Error code {this.state.errors}: unable to geocode
+          <Button variant="warning" onClick={()=>this.setState({showAlert: false})}>Close</Button>
+        </Alert>
+
+        <input type="text" onChange={this.handleInput} placeholder='Enter City Name Here'></input>
+        <Button varient="primary" onClick={this.getLocation}>Explore!</Button>
+        <h1>{this.state.locationData.display_name}</h1>
+        <p>Latitude: {this.state.locationData.lat}</p>
+        <p>Longitude: {this.state.locationData.lon}</p>
+        <Image src={this.state.map} rounded/>
+      
+        
+      </div>
     )
   }
 }
 
 
 export default App;
+
